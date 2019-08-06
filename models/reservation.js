dbfdde961411d518ef47c9586209914d8339bc00
Loydith Valdivia <loydith@gmail.com>
2019-08-06 18:34:16 -0500

    create the reservation

---
+++ models/reservation.js
@@ -0,0 +1,86 @@
+module.exports = function (sequelize, DataTypes) {
+        var Reservation = sequelize.define("Reservation", {
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            phone: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            check_in: {
+                type: DataTypes.Date,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            check_out: {
+                type: DataTypes.Date,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            
+            room_type: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            guests_adult: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            guests_child: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+    
+            room_qty: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            cr_qty: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+            message: {
+                type: DataTypes.STRING(1234),
+                allowNull: false,
+                validate: {
+                    len: [1]
+                }
+            },
+        });
+        return Reservation;
+    };
+    
