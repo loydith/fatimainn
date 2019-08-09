9916b0d289b76dd40fdaf3f6f4194b33400ee7ea
Loydith Valdivia <loydith@gmail.com>
2019-08-09 18:14:23 -0500

    create the api-route

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
 // require("dotenv").config();
 var session = require('express-session')
-const express = require ('express');
-const sendMail = require ('./mail');
+const express = require('express');
+const sendMail = require('./mail');
 const cookieParser = require('cookie-parser'); //hide the ids
 const SessionStore = require('express-session-sequelize')(session.Store);//create the store
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const db = require("./models");
-const Sequelize = require('sequelize');
-const config = require('./config/config.json');
 const passport = require('./passport/index.js');
-const dev = config['development'];
-var sequelize = new Sequelize(dev.database, dev.username, dev.password, {
-    host:'localhost',
-    dialect:'mysql'
-});
-const store = new SessionStore({
-    db: sequelize
-})
-
 // Satitic folder
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -28,15 +17,14 @@ var syncOptions = { force: false };
 // const auth = require('./config/passport')(passport);
 
 //data parsing
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(session({
-    secret: "fatima inn",
-    store,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false }
+    secret: 'asjdlaksjdajdlkajdljalksjd',
+    resave: true,
+    saveUninitialized: true,
+    cookie: { maxAge: 100 * 60 * 60 * 24 * 30 } // = 30 days
 }));
 
 //possport => make sure the user and pass saved 
@@ -45,89 +33,39 @@ app.use(passport.session());
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
 if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
+    syncOptions.force = true;
 }
 // email, subject, text
 app.post('/email', (req, res) => {
-//send email here
+    //send email here
 
-const {email, subject, text} = req.body;
-console.log('Data: ', req.body);
-sendMail(email, subject, text, function(err, data){
-    if(err){
-        res.status(500).json({ message: 'Internal Error'});
-    }else {
-        res.json({message: 'Email sent!!!'});
-        alert("Email has been sent!!!");
-    }
-});
-});
-
-
-// pages
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-app.get('/index', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
-app.get('/explore', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'explore.html'));
-});
-app.get('/auditorio', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'auditorio.html'));
-});
-app.get('/contactos', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'contactos.html'));
-});
-app.get('/diacompleto', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'diacompleto.html'));
-});
-app.get('/habitaciones', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'habitaciones.html'));
-});
-app.get('/mediodia', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'mediodia.html'));
+    const { email, subject, text } = req.body;
+    console.log('Data: ', req.body);
+    sendMail(email, subject, text, function (err, data) {
+        if (err) {
+            res.status(500).json({ message: 'Internal Error' });
+        } else {
+            res.json({ message: 'Email sent!!!' });
+            alert("Email has been sent!!!");
+        }
+    });
 });
 
-app.get('/paqueteturistico', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'paqueteturistico.html'));
-});
 
-app.get('/reservaciones', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'reservaciones.html'));
-});
-app.get('/login', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'login.html'));
-});
-app.get('/signup', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'views', 'signup.html'));
-});
-app.get('/dashboard', (req, res) =>{
-    console.log("user", req.user);
-    if (req.user) {
-        res.sendFile(path.join(__dirname, 'views', 'dashboard.html'));
-	} else {
-        res.sendFile(path.join(__dirname, 'views', 'login.html'));
-	}
-});
-  // SESSION SETUP
-  if (app.get("env") === "production") {
+// SESSION SETUP
+if (app.get("env") === "production") {
     app.set("trust proxy", 1); // trust first proxy
     sess.cookie.secure = true; // serve secure cookies
-  };
+};
 
 // Routes
 // require("./routes/api-routes")(app);
-// require("./routes/html-routes")(app);
+const htmlRoutes = require("./routes/html-routes");
 const auth = require('./routes/auth');
+app.use('/', htmlRoutes);
 app.use('/api/', auth);
-  db.sequelize.sync().then(function() {
-    app.listen(PORT, function() {
-      console.log("App listening on PORT " + PORT);
+db.sequelize.sync({ force: true }).then(function () {
+    app.listen(PORT, function () {
+        console.log("App listening on PORT " + PORT);
     });
-  });
-  
-
-
-
+});
