8686d76cce47820b6862cc4653866491d29b63ef
Valarie Cardoza <vjcardoza2011@yahoo.com>
2019-08-11 18:25:46 -0500

    component changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import Nav from './Nav';
+import Nav from './components/Nav/Nav';
 import Card from './components/IndexCard/Card';
-import Footer from './Footer';
-import Slider from './Slider';
-import Explore from './Explore';
-import Carousel from './Carousel';
-import Booking from './Booking';
-import Contact from './Contact';
-import CardContact from './CardContact';
-import Rooms from './Rooms';
-import Modal from './Modal';
-import Login from './Login';
-import Signup from './Signup';
-import Halfday from './Halfday';
-import Fullday from './Fullday';
-import ConferenceRoom from './ConferenceRoom';
-import SlideConference from './SlideConference';
+import Footer from './components/Footer/Footer';
+import Slider from './components/Slider/Slider';
+import Explore from './components/Explore/Explore';
+import Carousel from './components/Carousel/Carousel';
+import Booking from './components/Booking/Booking';
+import Contact from './components/Contact/Contact';
+import ContactCard from './components/ContactCard/ContactCard';
+import Rooms from './components/Rooms/Rooms';
+import Modal from './components/Modal/Modal';
+import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
+import Halfday from './components/Halfday/Halfday';
+import Fullday from './components/Fullday/Fullday';
+import ConferenceRoom from './components/ConferenceRoom/ConferenceRoom';
+import SlideConference from './components/SlideConference/SlideConference';
 
 import './App.css';
-import Package from './Package';
+import Package from './components/Package/Package';
 
 
 function App() {
