e94c52127dce282d0a0138794b6d5da24bcc1ff9
Loydith Valdivia <loydith@gmail.com>
2019-08-13 10:06:00 -0500

    change the path in app.js

--- client/src/pages/Auditorio/index.js
+++ client/src/pages/Auditorio/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Carousel from '../../components/Carousel'
 import Card from '../../components/ConferenceRoom';
+import SlideConference from '../../components/SlideConference/SlideConference';
 
-import SlideConference from '../components/SlideConference';
 export default class ConferenceRoom extends React.Component {
     render() {
         return (
--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import Carousel from '../../components/Carousel'
-import Card from '../../components/IndexCard';
+import Slider from '../../components/Slider/Slider'
+import IndexCard from '../../components/IndexCard/Card';
 export default class Home extends React.Component {
     render() {
         return (
             <React.Fragment>
-                <Carousel />
-                <Card />
+                <Slider />
+                <IndexCard />
             </React.Fragment>
         )
     }
